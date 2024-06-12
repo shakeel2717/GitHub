0edb6a13804e6668d438cd1d6b6a9042bbdf2a48
Shakeel Ahmad <shakeel2717@gmail.com>
2024-06-12 16:38:32 +0500

    Update web.php file, and remove meta tag from welcome file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My own Page</title>
 </head>
 <body>
     <h1>Welcome to My Github Page</h1>
+    <a href="#">Login Now</a>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+// This is routing file for web.php
+
 Route::get('/', function () {
     return view('welcome');
 });
